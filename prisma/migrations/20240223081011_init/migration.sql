5ca42ecb1a49fcaf3e8c688b1d1e30ce34dfabc2
tlqvst <erik@tellqvist.net>
2024-02-24 11:32:45 +0100

    chore: cleanup migrations

--- prisma/migrations/20240223081011_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Game" ALTER COLUMN "playtime" DROP NOT NULL;
--- prisma/migrations/20240224103206_init/migration.sql
+++ prisma/migrations/20240224103206_init/migration.sql
@@ -10,6 +10,16 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "UserSettings" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "background" VARCHAR(250),
+    "avatar" VARCHAR(250),
+
+    CONSTRAINT "UserSettings_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "Role" (
     "id" SERIAL NOT NULL,
     "displayName" TEXT NOT NULL,
@@ -33,12 +43,12 @@ CREATE TABLE "Game" (
     "date" TIMESTAMP(3) NOT NULL,
     "title" VARCHAR(80) NOT NULL,
     "system" VARCHAR(80) NOT NULL,
-    "background" VARCHAR(200) NOT NULL,
-    "playtime" VARCHAR(50) NOT NULL,
+    "background" VARCHAR(200),
+    "playtime" INTEGER,
     "status" VARCHAR(40) NOT NULL DEFAULT 'In Progress',
     "content" VARCHAR(1000) NOT NULL,
     "owned" BOOLEAN NOT NULL,
-    "achievementsLink" VARCHAR(200) NOT NULL,
+    "achievementsLink" VARCHAR(200),
     "perfectGame" BOOLEAN NOT NULL,
     "userId" INTEGER NOT NULL,
 
@@ -52,10 +62,16 @@ CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
 
 -- CreateIndex
+CREATE UNIQUE INDEX "UserSettings_userId_key" ON "UserSettings"("userId");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "Role_displayName_key" ON "Role"("displayName");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Role_name_key" ON "Role"("name");
+
+-- AddForeignKey
+ALTER TABLE "UserSettings" ADD CONSTRAINT "UserSettings_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "RolesOnUser" ADD CONSTRAINT "RolesOnUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
